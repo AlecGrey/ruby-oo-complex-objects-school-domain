1e353ce3d9b846b74adad8fed5846f83edd90018
AlecGrey <alecgrey.coding@gmail.com>
2020-10-08 13:42:09 -0700

    finished lib/school.rb

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,34 @@
 # code here!
+require 'pry'
+
+class School
+
+    attr_reader :name, :roster
+
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] = [] if @roster[grade] == nil
+        @roster[grade] << name
+    end
+
+    def grade(num)
+        @roster[num]
+    end
+
+    def sort
+        roster = @roster.dup
+        roster.each do |k,v|
+            roster[k] = roster[k].sort
+        end
+        roster
+    end
+
+
+end
+
+# nautilus = School.new("nautilus")
+# nautilus.add_student("Alec Grey", 1)
